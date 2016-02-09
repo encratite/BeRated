425ebc283c2fa519e6e33139357907f88d0e12f5
void <git.email.address@googlemail.com>
2016-02-09 18:51:58 +0100

    Did some cleaning up in the cache manager

--- BeRated/Cache/CacheManager.cs
+++ BeRated/Cache/CacheManager.cs
@@ -42,10 +42,13 @@ namespace BeRated.Cache
 		private Dictionary<string, Team> _PlayerTeams = null;
 		private List<Round> _Rounds = new List<Round>();
 
+        private LogParser _LogParser;
+
 		public CacheManager(string logPath, string connectionString)
 		{
 			_LogPath = logPath;
 			_ConnectionString = connectionString;
+            _LogParser = new LogParser(this);
 		}
 
 		public void Dispose()
@@ -126,115 +129,144 @@ namespace BeRated.Cache
 				int lineCounter = 1;
 				foreach (var line in lines)
 				{
-					ProcessLine(line, lineCounter);
+					ProcessLine(line);
 					lineCounter++;
 				}
 				_LogStates[fileName] = currentFileSize;
 			}
 		}
 
-		private void ProcessLine(string line, int lineCounter)
-		{
-			var logParser = new LogParser(this);
-			var kill = logParser.ReadPlayerKill(line);
-			if (kill != null)
-			{
-				if (kill.Killer.SteamId == LogParser.BotId || kill.Victim.SteamId == LogParser.BotId || kill.Killer == kill.Victim)
-					return;
-				kill.Killer.Kills.Add(kill);
-				kill.Victim.Deaths.Add(kill);
-				return;
-			}
-			int? maxRounds = logParser.ReadMaxRounds(line);
-			if (maxRounds != null)
-			{
-				_MaxRounds = maxRounds.Value;
-				return;
-			}
-			var teamSwitch = logParser.ReadTeamSwitch(line);
-			if (teamSwitch != null)
-			{
-				string steamId = teamSwitch.Player.SteamId;
-				var team = teamSwitch.CurrentTeam;
-				if (steamId == LogParser.BotId)
-					return;
-				_PlayerTeams[steamId] = team;
-				return;
-			}
-			var disconnect = logParser.ReadDisconnect(line);
-			if (disconnect != null)
-			{
-				string steamId = disconnect.Player.SteamId;
-				if (steamId == LogParser.BotId)
-					return;
-				_PlayerTeams.Remove(steamId);
-			}
-			var round = logParser.ReadEndOfRound(line);
-			if (round != null)
-			{
-				if (round.TerroristScore == 0 && round.CounterTerroristScore == 0)
-					return;
-				int roundsPlayed = round.TerroristScore + round.CounterTerroristScore;
-				if (roundsPlayed == 1)
-					_Rounds.Clear();
-				_Rounds.Add(round);
-				var winningTeam = logParser.GetWinningTeam(round.SfuiNotice);
-				foreach (var pair in _PlayerTeams)
-				{
-					var player = _Players[pair.Key];
-					var playerTeam = pair.Value;
-					var container = playerTeam == winningTeam ? player.RoundsWon : player.RoundsLost;
-					container.Add(round);
-				}
-				int roundsToWin = _MaxRounds / 2 + 1;
-				bool terroristsWinGame = round.TerroristScore >= roundsToWin;
-				bool counterTerroristsWinGame = round.CounterTerroristScore >= roundsToWin;
-				bool draw = roundsPlayed >= _MaxRounds;
-				if (terroristsWinGame || counterTerroristsWinGame || draw)
-				{
-                    var outcome = GameOutcome.Draw;
-                    if (terroristsWinGame)
-                        outcome = GameOutcome.TerroristsWin;
-                    else if (counterTerroristsWinGame)
-                        outcome = GameOutcome.CounterTerroristsWin;
-					var game = new Game(_Rounds, outcome);
-                    _Games.Add(game);
-					foreach (var pair in _PlayerTeams)
-					{
-						var player = _Players[pair.Key];
-						var playerTeam = pair.Value;
-						List<Game> games;
-						if (draw)
-						{
-							games = player.Draws;
-						}
-						else if (
-							(terroristsWinGame && playerTeam == Team.Terrorist) ||
-							(counterTerroristsWinGame && playerTeam == Team.CounterTerrorist)
-						)
-						{
-							games = player.Wins;
-						}
-						else
-						{
-							games = player.Losses;
-						}
-						games.Add(game);
-                        var players = playerTeam == Team.Terrorist ? game.Terrorists : game.CounterTerrorists;
-                        players.Add(player);
-					}
-				}
-				return;
-			}
-			var purchase = logParser.ReadPurchase(line);
-			if (purchase != null)
-			{
-				string steamId = purchase.Player.SteamId;
-				if (steamId == LogParser.BotId)
-					return;
-				purchase.Player.Purchases.Add(purchase);
-				return;
-			}
+		private void ProcessLine(string line)
+        {
+            var readers = new Func<string, bool>[]
+            {
+                ReadPlayerKill,
+                ReadMaxRounds,
+                ReadTeamSwitch,
+                ReadDisconnect,
+                ReadEndOfRound,
+                ReadPurchase
+            };
+            foreach (var reader in readers)
+            {
+                bool done = reader(line);
+                if (done)
+                    return;
+            }
 		}
-	}
+
+        #region Line processing methods
+
+        private bool ReadPlayerKill(string line)
+        {
+            var kill = _LogParser.ReadPlayerKill(line);
+			if (kill == null)
+                return false;
+			if (kill.Killer.SteamId == LogParser.BotId || kill.Victim.SteamId == LogParser.BotId || kill.Killer == kill.Victim)
+				return true;
+			kill.Killer.Kills.Add(kill);
+			kill.Victim.Deaths.Add(kill);
+			return true;
+        }
+
+        private bool ReadMaxRounds(string line)
+        {
+            int? maxRounds = _LogParser.ReadMaxRounds(line);
+			if (maxRounds == null)
+                return false;
+			_MaxRounds = maxRounds.Value;
+            return true;
+        }
+
+        private bool ReadTeamSwitch(string line)
+        {
+            var teamSwitch = _LogParser.ReadTeamSwitch(line);
+			if (teamSwitch == null)
+                return false;
+			string steamId = teamSwitch.Player.SteamId;
+			var team = teamSwitch.CurrentTeam;
+			if (steamId != LogParser.BotId)
+			    _PlayerTeams[steamId] = team;
+            return true;
+        }
+
+        private bool ReadDisconnect(string line)
+        {
+            var disconnect = _LogParser.ReadDisconnect(line);
+			if (disconnect == null)
+                return false;
+			string steamId = disconnect.Player.SteamId;
+			if (steamId != LogParser.BotId)
+				_PlayerTeams.Remove(steamId);
+			return true;
+        }
+
+        private bool ReadEndOfRound(string line)
+        {
+            var round = _LogParser.ReadEndOfRound(line);
+            if (round == null || round.TerroristScore == 0 && round.CounterTerroristScore == 0)
+                return false;
+            int roundsPlayed = round.TerroristScore + round.CounterTerroristScore;
+            if (roundsPlayed == 1)
+                _Rounds.Clear();
+            _Rounds.Add(round);
+            var winningTeam = _LogParser.GetWinningTeam(round.SfuiNotice);
+            foreach (var pair in _PlayerTeams)
+            {
+                var player = _Players[pair.Key];
+                var playerTeam = pair.Value;
+                var container = playerTeam == winningTeam ? player.RoundsWon : player.RoundsLost;
+                container.Add(round);
+            }
+            CheckForEndOfGame(round, roundsPlayed);
+            return true;
+        }
+
+        private bool ReadPurchase(string line)
+        {
+            var purchase = _LogParser.ReadPurchase(line);
+			if (purchase == null)
+                return false;
+			string steamId = purchase.Player.SteamId;
+			if (steamId != LogParser.BotId)
+			    purchase.Player.Purchases.Add(purchase);
+            return true;
+        }
+
+        #endregion
+
+        private void CheckForEndOfGame(Round round, int roundsPlayed)
+        {
+            int roundsToWin = _MaxRounds / 2 + 1;
+            bool terroristsWinGame = round.TerroristScore >= roundsToWin;
+            bool counterTerroristsWinGame = round.CounterTerroristScore >= roundsToWin;
+            bool draw = roundsPlayed >= _MaxRounds;
+            if (!(terroristsWinGame || counterTerroristsWinGame || draw))
+                return;
+            var outcome = GameOutcome.Draw;
+            if (terroristsWinGame)
+                outcome = GameOutcome.TerroristsWin;
+            else if (counterTerroristsWinGame)
+                outcome = GameOutcome.CounterTerroristsWin;
+            var game = new Game(_Rounds, outcome);
+            _Games.Add(game);
+            foreach (var pair in _PlayerTeams)
+            {
+                var player = _Players[pair.Key];
+                var playerTeam = pair.Value;
+                List<Game> games;
+                bool terroristWin = terroristsWinGame && playerTeam == Team.Terrorist;
+                bool counterTerroristWin = counterTerroristsWinGame && playerTeam == Team.CounterTerrorist;
+                if (draw)
+                    games = player.Draws;
+                else if (terroristWin || counterTerroristWin)
+                    games = player.Wins;
+                else
+                    games = player.Losses;
+                games.Add(game);
+                var players = playerTeam == Team.Terrorist ? game.Terrorists : game.CounterTerrorists;
+                players.Add(player);
+            }
+        }
+    }
 }
