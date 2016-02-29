48047f37de214b535e480233c8529a8755cc844b
void <git.email.address@googlemail.com>
2016-02-29 01:49:59 +0100

    Added missing file

---
+++ BeRated.Server/Cache/PlayerGameState.cs
@@ -0,0 +1,24 @@
+﻿using Moserware.Skills;
+using Team = BeRated.Common.Team;
+
+namespace BeRated.Cache
+{
+    class PlayerGameState
+    {
+        public Team Team { get; set; }
+
+        public Rating PreGameMatchRating { get; set; }
+
+        public Rating PreGameKillRating { get; set; }
+
+        public int? RoundPlayerLeft { get; set; }
+
+        public PlayerGameState(Player player, Team team)
+        {
+            Team = team;
+            PreGameMatchRating = player.MatchRating;
+            PreGameKillRating = player.KillRating;
+            RoundPlayerLeft = null;
+        }
+    }
+}
