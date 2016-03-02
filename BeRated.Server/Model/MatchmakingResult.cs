ba4d505541e48e06d859ed344d76eb33e770280d
void <git.email.address@googlemail.com>
2016-03-02 17:58:55 +0100

    Added missing file

---
+++ BeRated.Server/Model/MatchmakingResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BeRated.Model
+{
+    public class MatchmakingResult
+    {
+        public double Quality { get; set; }
+
+        public List<PlayerInfo> Team1 { get; set; }
+        public List<PlayerInfo> Team2 { get; set; }
+    }
+}
