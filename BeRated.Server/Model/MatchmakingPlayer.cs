55c91893255f62c3f6e014adbbd7a28be7f70796
void <git.email.address@googlemail.com>
2016-03-06 21:01:35 +0100

    Added missing file

---
+++ BeRated.Server/Model/MatchmakingPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BeRated.Model
+{
+    public class MatchmakingPlayer : PlayerInfo
+    {
+        public double MatchRating { get; set; }
+        public int Games { get; set; }
+        public DateTime? LastRound { get; set; }
+    }
+}
