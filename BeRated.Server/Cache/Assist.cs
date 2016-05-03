8090bb6fd766d2f6f497050a95f645e57aa65624
void <git.email.address@googlemail.com>
2016-05-03 20:32:59 +0200

    Added missing file

---
+++ BeRated.Server/Cache/Assist.cs
@@ -0,0 +1,14 @@
+﻿using BeRated.Common;
+using System;
+
+namespace BeRated.Cache
+{
+	class Assist
+	{
+		public DateTime Time { get; set; }
+		public Player Assistant { get; set; }
+		public Team AssistantTeam { get; set; }
+		public Player Victim { get; set; }
+		public Team VictimTeam { get; set; }
+	}
+}
